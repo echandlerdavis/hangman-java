d778b4c7b8da49556688ae68651b55538b6b4e0e
Chandler Davis <echandlerdavis@gmail.com>
2023-01-02 19:28:48 -0800

    wrote function getRandomWord(), made outline of what to do next

--- Hangman.java
+++ Hangman.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hangman {
 
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
@@ -67,14 +69,50 @@ public class Hangman {
     " =========\n"};
 
     public static void main(String[] args) {
-        
 
+        Scanner scan = new Scanner(System.in);
         System.out.println("Let's play hangman! Press any key to continue");
+        scan.nextLine();
+        int misses = 0;
+
+        // initialize board
+        // Get a random word with the random word generator
+        // Make a function that prints the board (based off of tic tac toe);
+            // if misses are a certain number, prints different board. 
+            // In here make sure that it prints the length of the word? Unless that needs to be in the main part
+        // Make a function to ask user, and compares the character to the characters in the string(Since a string is just a different kind of array)
+        // Adds misses if incorrect, and updates board, fills in spaces if correct and displays board
+        // If they get the word, they win
+        // If the gallows gets to the end, they lose. 
+
+        
+
+        System.out.println(getRandomWord());
+
+
+
+        scan.close();
         
 
 
 
 
+    };
+    /**
+     * Function name - getRandomWord
+     * @return word (String)
+     * 
+     * Inside the function: 
+     *      1. 
+     */
+
+    public static String getRandomWord(){
+        // get a random number and choose from the list based on that number. 
+        int randomNumber = (int)(Math.random() * words.length);
+
+        String word = words[randomNumber]; 
+
+        return word;
     }
 
 }
